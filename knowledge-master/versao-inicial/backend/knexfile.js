7c7390c31f163870775ff1486336fe60195d537d
Ricardo Santos <ricardo.eletris@hotmail.com>
2020-06-28 21:49:52 -0300

    dwd
    dwwd

--- knowledge-master/versao-inicial/backend/knexfile.js
+++ knowledge-master/versao-inicial/backend/knexfile.js
@@ -1,7 +1,7 @@
 // esse arquivo é gerado com terminal: knex init dentro da pasta backend
 // usar tbm o knex migrate:make create_table_users para criar a pasta migrations depois knex migrate:make create_table_categories depois knex migrate:make create_table_articles
 
-//para o postgres 12 segir criação do user e dar grante ao user na tabela knowwledge
+//para o postgres 12 segir criação do user e dar grante ao user na tabela knowledge
 
 // após configurar as migratiosn rodar knex migrate:latest ou rollback para voltar
 // Update with your config settings.
